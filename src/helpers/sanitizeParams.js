01e2c71b8facdb9a2109fdda37037e299595e0c9
Aurélien Dudonney <a.dudonney@gmail.com>
2021-07-01 11:58:04 +0700

    upgrade packages

--- src/helpers/sanitizeParams.js
+++ src/helpers/sanitizeParams.js
@@ -3,10 +3,6 @@
  * @return {array|undefined}
  */
 export default function (params) {
-  // is an array
   if (Array.isArray(params)) return params
-  // create an array
-  const arr = []
-  arr.push(params)
-  return arr
+  return [...params]
 }
